51103e55e363aed73533bc046cf121f69eeb4baf
Kiyoko Ikeuchi <kiyoko.ikeuchi@gmail.com>
2016-08-22 08:59:03 -0700

    style: Run lint on palindromic_substring.py

--- palindromic_substring.py
+++ palindromic_substring.py
@@ -4,24 +4,26 @@ Given a string a, find the longest palindromic substring contained
 in a. Your function definition should look like question2(a), and 
 return a string
 """
-### Question2 main code
+# Question2 main code
+
+
 def question2(a):
     if len(a) == 0:
         print "Invalid inputs!"
     else:
-        a = a.lower()        
+        a = a.lower()
         longest = a[0]
         palindrome = a[0]
         for i in range(len(a)):
             for j in range(i):
-                letters = a[j:i+1]
+                letters = a[j:i + 1]
                 if letters == letters[::-1]:
                     palindrome = letters
                     if len(palindrome) > len(longest):
                         longest = palindrome
         return longest
 
-### Question2 test cases
+# Question2 test cases
 # Case 1: good example
 a = 'bananas'
 print question2(a)
@@ -36,4 +38,3 @@ print question2(a)
 a = 'RACECAR'
 print question2(a)
 # Expected output is an error message: "Invalid inputs!"
-
